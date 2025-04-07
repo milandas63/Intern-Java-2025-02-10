70499ed804a76b74c679444cc2b6df1e680359b0
Milan Das <45796126+milandas63@users.noreply.github.com>
2025-04-07 12:36:24 +0530

    Add files via upload

---
+++ Day-28 (07-04-2025)/Example.java
@@ -0,0 +1,22 @@
+package com.oops;
+
+public class Example {
+    public void main() {
+    }
+
+    public void main(String arg) {
+    }
+
+    public void main(String arg, int x) {
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
+
+class Sample extends Example {
+	@Override
+	public void main(String args) {
+    }
+}
